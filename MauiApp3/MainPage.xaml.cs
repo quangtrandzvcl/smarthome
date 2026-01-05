051e08bba754082677b5e75c701ad066b469114e
ThanhTrung-05 <thanhtrung17112005@gmail.com>
2026-01-05 10:20:05 +0700

    Add standard .NET gitignore

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
